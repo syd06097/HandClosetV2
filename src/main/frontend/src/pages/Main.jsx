474797ef4d7a156063bca39f4bcf60b5cda9b162
Kim Chan Ho <chgim1128@gmail.com>
2023-05-31 12:55:44 +0900

    chan
    main 위치정보 추가

--- src/main/frontend/src/pages/Main.jsx
+++ src/main/frontend/src/pages/Main.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Main = () => {
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+
+  useEffect(() => {
+    // 위치 정보 가져오기
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLatitude(position.coords.latitude);
+          setLongitude(position.coords.longitude);
+        },
+        (error) => {
+          console.error("Error getting geolocation:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
   return (
     <div>
       <h1>Main</h1>
+      {latitude && longitude && (
+        <p>
+          Your current location: {latitude}, {longitude}
+        </p>
+      )}
     </div>
   );
 };
